a36f594ae4e9a82d9f6b81f639c5e0ac527be8d6
OWAIS KHAN <162675787+iamowais7@users.noreply.github.com>
2025-02-19 11:41:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Intersection Sorted Linked Lists/intersection-sorted-linked-lists.cpp
@@ -0,0 +1,116 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printList(struct Node *head) {
+    struct Node *temp = head;
+    while (temp != NULL) {
+        cout << temp->data << ' ';
+        temp = temp->next;
+    }
+    cout << '\n';
+}
+
+
+// } Driver Code Ends
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+    Node* findIntersection(Node* head1, Node* head2) {
+        if(head1 == NULL || head2 == NULL) return NULL;
+        
+        Node * list = new Node(0);
+        Node * head = list;
+        
+        while(head1 && head2){
+            if(head1->data < head2->data){
+                head1 = head1->next;
+            }
+            else if(head1->data > head2->data){
+                head2 = head2->next;
+            }
+            else{
+                head->next = new Node(head1->data);
+                head = head->next;
+                head1 = head1->next;
+                head2 = head2->next;
+            }
+        }
+        return list->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+    int T;
+    cin >> T;
+    cin.ignore();
+    while (T--) {
+        int n1, n2, tmp;
+        Node *head1 = nullptr, *tail1 = nullptr;
+        Node *head2 = nullptr, *tail2 = nullptr;
+        string input1, input2;
+
+        getline(cin, input1); // Read the entire line for LL1 elements
+        stringstream ss1(input1);
+        while (ss1 >> tmp) {
+            Node *new_node = new Node(tmp);
+            if (head1 == nullptr) {
+                head1 = new_node;
+                tail1 = new_node;
+            } else {
+                tail1->next = new_node;
+                tail1 = new_node;
+            }
+        }
+
+        getline(cin, input2); // Read the entire line for LL2 elements
+        stringstream ss2(input2);
+        while (ss2 >> tmp) {
+            Node *new_node = new Node(tmp);
+            if (head2 == nullptr) {
+                head2 = new_node;
+                tail2 = new_node;
+            } else {
+                tail2->next = new_node;
+                tail2 = new_node;
+            }
+        }
+
+        Solution obj;
+        Node *result = obj.findIntersection(head1, head2);
+
+        printList(result);
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
